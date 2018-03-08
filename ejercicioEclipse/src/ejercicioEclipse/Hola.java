4ea82afaae7a4d28c7b863cf227a7c41a72b77d4
Roberto Oliveira <oliroberto@gmail.com>
2018-03-08 17:15:33 +0100

    nueva linea

--- ejercicioEclipse/src/ejercicioEclipse/Hola.java
+++ ejercicioEclipse/src/ejercicioEclipse/Hola.java
@@ -4,8 +4,9 @@ public class Hola {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("HOLA, que tal? Me has escuchado");
-		System.out.println("¿y tu?");
+		System.out.println("HOLA, que tal?");
+		System.out.println("Me has escuchado");
+		System.out.println("Â¿y tu?");
 	}
 
 }
