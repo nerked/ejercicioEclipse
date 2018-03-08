ac42f2d38466e2655a17ef8fc1be062e0a57d0cc
Roberto Oliveira <oliroberto@gmail.com>
2018-03-08 17:22:26 +0100

    corregido conflicto

--- ejercicioEclipse/src/ejercicioEclipse/Hola.java
+++ ejercicioEclipse/src/ejercicioEclipse/Hola.java
@@ -4,7 +4,8 @@ public class Hola {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("HOLA, que tal? Me has escuchado");
+		System.out.println("HOLA, que tal?");
+		System.out.println("Me has escuchado");
 		System.out.println("¿y tu?");
 		System.out.println("probando conflictos");
 	}
